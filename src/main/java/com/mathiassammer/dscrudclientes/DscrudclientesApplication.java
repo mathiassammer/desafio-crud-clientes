ea6089d06cbad4fea859cad3ad68b7428f999fcd
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-22 10:38:57 -0300

    first commit

---
+++ src/main/java/com/mathiassammer/dscrudclientes/DscrudclientesApplication.java
@@ -0,0 +1,13 @@
+package com.mathiassammer.dscrudclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscrudclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscrudclientesApplication.class, args);
+	}
+
+}
